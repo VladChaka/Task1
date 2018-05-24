42b318331ccc485b5624f073c942a987219b40c7
Vladislav Chugunov <vladislav.chugunov@VCHUGUNOV.intexsoft.by>
2018-05-24 10:52:32 +0300

    Optimized code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function reSize(ev) {
 	let body = document.getElementById("form1");
 	let btnReSize = document.getElementById("resize");	
 
-	btnReSize.onmousedown = function(ev) {
+	function mouseDown (ev) {
 		
 		function move(ev) {
 			let left = body.style.left;
@@ -11,55 +11,61 @@ function reSize(ev) {
 			let top = body.style.top;
 				top = top.split("px")[0];
 				
-			body.style.width = ev.x + 5 - left + "px";
-			body.style.height = ev.y + 5 - top + "px";					
+			body.style.width = ev.x + btnReSize.offsetWidth / 2 - left + "px";
+			body.style.height = ev.y + btnReSize.offsetHeight / 2 - top + "px";					
 		}
 
-		document.onmousemove = function(ev) {
-			move(ev);
+		let mouseMove = function(event) {
+			move(event);
 		}
+		document.addEventListener("mousemove", mouseMove);
 
-		btnReSize.onmouseup = function() {
-			document.onmousemove = null;
-			btnReSize.onmouseup = null;
+		let mouseUp = function() {
+			document.removeEventListener("mousemove", mouseMove);
+			btnReSize.removeEventListener("mouseup", mouseUp);
 		}
-		body.onmouseup = function() {
-			document.onmousemove = null;
-			body.onmousemove = null;
-			body.onmouseup = null;
-			btnReSize.onmouseup = null;
-		}
+		document.addEventListener("mouseup", mouseUp);
 	}
+	btnReSize.addEventListener("mousedown", mouseDown);
 }
 
-function dragAndDrop(ev) {
-	let body = document.getElementById("form1");
-	let headBlock = document.getElementById("head");
-		  
-	headBlock.onmousedown = function(ev) {
+function dragAndDrop(event) {
+	let body = document.getElementById("form1"),
+		headBlock = document.getElementById("head");
+		
+	let mouseDown = function(event) {
+		function move(event) {
+			
+			let left = body.style.left,
+				top = body.style.top;
 
-		function move(ev) {
-			if (ev.pageX > -1 && ev.pageY > -1) {
-				body.style.left = ev.x - headBlock.offsetWidth / 2 + "px";
-				body.style.top = ev.y - headBlock.offsetHeight / 2 + "px";
+			if (event.x > -1 && event.y > -1) {
+				left = event.x - headBlock.offsetWidth / 2 + "px";
+				top = event.y - headBlock.offsetHeight / 2 + "px";
 			} 
-			else if (ev.pageY > -1 && ev.pageY < -1) {
-				body.style.left = ev.x - headBlock.offsetWidth / 2 + "px";
-				body.style.top = 0 + "px";
-			} 
-			else if (ev.pageY < -1 && ev.pageY > -1) {
-				body.style.left = 0 + "px";
-				body.style.top = ev.y - headBlock.offsetHeight / 2 + "px";
+			else if (event.x > -1 && event.y < -1) {
+				left = event.x - headBlock.offsetWidth / 2 + "px";
+				top = 0 + "px";
 			}
+		
+			body.style.left = left;
+			body.style.top = top;
 		}
+		
+		let mouseMove = function(event) {
+			move(event);
+		}
+		document.addEventListener("mousemove", mouseMove);
 
-		document.onmousemove = function(ev) {
-			move(ev);
+		let mouseUp = function() {
+			document.removeEventListener("mousemove", mouseMove);
+			headBlock.removeEventListener("mouseup", mouseUp);
 		}
-
-		body.onmouseup = function() {
-			document.onmousemove = null;
-			headBlock.onmouseup = null;
-		}
+		document.addEventListener("mouseup", mouseUp);
+		
 	}
+	headBlock.addEventListener("mousedown", mouseDown);
 }
+
+reSize(event);
+dragAndDrop(event);
